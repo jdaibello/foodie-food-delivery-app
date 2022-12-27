fa1f86196ce5ef51c3a38c06dd6d2bc1da2f456c
João Pedro <jpedrodaibello1999@outlook.com>
2022-12-27 17:26:02 -0300

    feat: change default app font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodie/app/modules/spash/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Foodie - Food Delivery App',
+      theme: ThemeData(fontFamily: 'NotoSans'),
       debugShowCheckedModeBanner: false,
-      home: Container(color: Colors.white),
+      home: const SplashPage(),
     );
   }
 }
