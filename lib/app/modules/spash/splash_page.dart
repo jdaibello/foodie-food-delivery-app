a2f0d5399a928de8c26297770aa49a352b3d4dfd
João Pedro <jpedrodaibello1999@outlook.com>
2022-12-27 18:31:14 -0300

    feat: add Splash Page

--- lib/app/modules/spash/splash_page.dart
+++ lib/app/modules/spash/splash_page.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:foodie/app/core/ui/extensions/size_screen_extension.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Splash Page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 152.w,
+              height: 156.h,
+              fit: BoxFit.fill,
+            ),
+            SizedBox(height: 159.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  'Good Food Is What We ',
+                  style: TextStyle(
+                    color: Color(0xFF505050),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  'Deliver',
+                  style: TextStyle(
+                    color: Color(0xFFEE555A),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
